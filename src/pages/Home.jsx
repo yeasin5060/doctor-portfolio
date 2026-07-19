3f4b8449baa1246b2fe0982f0d67401294ac25ac
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-19 13:58:04 +0600

    complete the HomeServiceSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeHeroSection from '../components/HomeHeroSection'
 import HomeWhySection from '../components/HomeWhySection'
+import HomeServiceSection from '../components/HomeServiceSection'
 
 const Home = () => {
   return (
     <div>
         <HomeHeroSection/>
         <HomeWhySection/>
+        <HomeServiceSection/>
     </div>
   )
 }
