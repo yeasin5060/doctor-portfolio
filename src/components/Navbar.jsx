f5471b345b57b5ba368a3fd6d6e1b24b3a89b921
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-19 13:47:00 +0600

    complete the HomeHeroSection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,17 @@ const Navbar = () => {
     <nav className=" bg-slate-50">
       <div className="mx-6">
           <div className="flex items-center justify-between max-w-7xl mx-auto py-4  transition-all">
-            <Link href="/" className="relative text-3xl font-medium text-slate-700">
-                 Dr.Sharmin
+            <Link href="/" className="text-3xl font-medium text-slate-700 border border-slate-400 py-1 px-8 rounded-2xl">
+              Dr.Sharmin
             </Link>
 
               {/* Desktop Menu */}
-            <div className="hidden sm:flex items-center gap-4 lg:gap-8 text-slate-600">
+            <div className="hidden sm:flex items-center gap-4 lg:gap-8 text-slate-600 border border-slate-400 py-2 px-8 rounded-2xl">
               <Link className='text-4 text-slate-600 font-bold capitalize' to='/'>Home</Link>
               <Link className='text-4 text-slate-600 font-bold capitalize' to="/about">About</Link>
               <Link className='text-4 text-slate-600 font-bold capitalize' to="/service">Service</Link>
             </div>
-            <button className="flex items-center gap-2 px-7 py-1.5 bg-green-500 hover:bg-green-600 text-sm transition text-white rounded-full">
+            <button className=" text-4 flex items-center gap-2 px-7 py-2 bg-green-500 hover:bg-green-600 text-sm transition text-white rounded-full">
               <PhoneCallIcon size={18} />
                 Contact
             </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HomeHeroSection from '../components/HomeHeroSection'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <HomeHeroSection/>
+    </div>
   )
 }
 
