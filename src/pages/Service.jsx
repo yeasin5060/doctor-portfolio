e2206d933144f062eb0a709c58bf8411e265c500
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-20 11:11:41 +0600

    complete the serviceContact

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ServiceBanner from '../components/ServiceBanner'
 import Services from '../components/Services'
+import ServiceContact from '../components/ServiceContact'
 
 const Service = () => {
   return (
     <div>
         <ServiceBanner/>
         <Services/>
+        <ServiceContact/>
     </div>
   )
 }
