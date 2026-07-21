70b9db8c10057d725e2f76507c83614e9e731c64
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-07-21 14:45:12 +0600

    complete the navbar bangla

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,40 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className=" bg-slate-50">
+    <nav className="bg-slate-50">
       <div className="mx-6">
-          <div className="flex items-center justify-between max-w-7xl mx-auto py-4  transition-all">
-            <Link href="/" className="text-3xl font-medium text-slate-700 border border-slate-400 py-1 px-8 rounded-2xl">
-              Dr.Sharmin
+        <div className="flex items-center justify-between max-w-7xl mx-auto py-4">
+
+          {/* Logo */}
+          <Link
+            to="/"
+            className="text-3xl font-medium text-slate-700 border border-slate-400 py-1 px-8 rounded-2xl"
+          >
+            ডা. শারমিন
+          </Link>
+
+          {/* Desktop Menu */}
+          <div className="hidden sm:flex items-center gap-4 lg:gap-8 text-slate-600 border border-slate-400 py-2 px-8 rounded-2xl">
+            <Link className="font-semibold" to="/">
+              হোম
             </Link>
 
-              {/* Desktop Menu */}
-            <div className="hidden sm:flex items-center gap-4 lg:gap-8 text-slate-600 border border-slate-400 py-2 px-8 rounded-2xl">
-              <Link className='text-4 text-slate-600 font-bold capitalize' to='/'>Home</Link>
-              <Link className='text-4 text-slate-600 font-bold capitalize' to="/about">About</Link>
-              <Link className='text-4 text-slate-600 font-bold capitalize' to="/service">Service</Link>
-            </div>
-            <button className=" text-4 flex items-center gap-2 px-7 py-2 bg-green-500 hover:bg-green-600 text-sm transition text-white rounded-full">
-              <PhoneCallIcon size={18} />
-                Contact
-            </button>
+            <Link className="font-semibold" to="/about">
+              আমার সম্পর্কে
+            </Link>
+
+            <Link className="font-semibold" to="/service">
+              সেবাসমূহ
+            </Link>
           </div>
+
+          {/* Contact Button */}
+          <button className="flex items-center gap-2 px-7 py-3 bg-blue-600 hover:bg-blue-700 transition text-white rounded-full">
+            <PhoneCallIcon size={18} />
+            যোগাযোগ করুন
+          </button>
+
+        </div>
       </div>
     </nav>
   )
